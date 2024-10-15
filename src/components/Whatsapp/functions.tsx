e1b5332b72ff2a759f68fe7ba10528381f04b0a1
Avichay Vaknin <Avichay.Vaknin@audiocodes.com>
2024-10-16 02:31:12 +0300

    now whatsapp is with uri encoding

--- src/components/Whatsapp/functions.tsx
+++ src/components/Whatsapp/functions.tsx
@@ -1,5 +1,8 @@
 import { TFunction } from "i18next";
 
 export function openWhatsapp(t: TFunction<"common", undefined>, isSchedule: boolean = false) {
-	window.open(`https://wa.me/?text=${t(`common:Whatsapp.${isSchedule ? "schedule" : "interested"}`)}`, "_blank");
+	window.open(
+		`https://wa.me/?text=${encodeURI(t(`common:Whatsapp.${isSchedule ? "schedule" : "interested"}`))}`,
+		"_blank"
+	);
 }
